2b0d4ccb9ba9afaab0e804181a3ec8d87e4ccdae
Kuruba Bulle Bhuvan <kbbhuvan2003@gmail.com>
2024-06-30 11:07:46 +0530

    pojo class in kotlin

--- app/src/main/java/Student.kt
+++ app/src/main/java/Student.kt
@@ -1,41 +1,5 @@
 /**
  * POJO = plain old java object, VO = value object
  */
-public Class student {
-    String name;
-    int age;
-    String address;
-
-    //ctrl+shift+A -- shortcut for all shortcuts
-
-
-    public student(String name, int age, String address) {
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-}
+class student //ctrl+shift+A -- shortcut for all shortcuts
+    (var name: String, var age: Int, var address: String)
